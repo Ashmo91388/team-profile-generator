c748294ea34e9dd38c400e624a03e16fbcebe5bb
cameronmcnz~clear <ashly_mossette@yahoo.com>
2023-06-27 17:30:59 -0700

    copied code structure from Manager.js to Engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,9 +1,21 @@
 const Employee = require('./Employee');
 
-class Engineer extends Employee {
+class Engineer {
   constructor(name, id, email, github) {
-    super(name, id, email);
+    this.employee = new Employee(name, id, email);
     this.github = github;
+  }
+
+  getName() {
+    return this.employee.getName();
+  }
+
+  getId() {
+    return this.employee.getId();
+  }
+
+  getEmail() {
+    return this.employee.getEmail();
   }
 
   getRole() {
